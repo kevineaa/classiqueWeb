94830c9d916630e3c201a14f4860ea0bdc6a40ff
kevineaa <theperfectjoke@gmail.com>
2018-05-29 12:50:43 +0200

    f

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -9,12 +9,22 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+use AppBundle\Entity\abonne;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 class SecurityController extends Controller
 {
-	    /**
+      /**
      * @Route("/login", name="login")
      */
-  public function login(Request $request, AuthenticationUtils $authenticationUtils)
+  /* public function login(Request $request, AuthenticationUtils $authenticationUtils)
 {
     // get the login error if there is one
     $error = $authenticationUtils->getLastAuthenticationError();
@@ -26,5 +36,36 @@ class SecurityController extends Controller
         'last_username' => $lastUsername,
         'error'         => $error,
     ));
-}
+}  */
+ public function loginAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('replace_with_some_route');
+        }
+
+        return $this->render(
+            'abonne/index.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+  
 }
